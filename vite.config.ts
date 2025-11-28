f8e23cda036e5fe27b0d627c169311b8b9c84ac7
SuperControlDev <supercontrol.dev@gmail.com>
2025-11-28 17:35:55 +0900

    Login logic updated

--- vite.config.ts
+++ vite.config.ts
@@ -15,10 +15,24 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path, // 保持路径不变
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
       },
       '/socket.io': {
         target: 'http://localhost:8080',
         ws: true,
+        changeOrigin: true,
       },
     },
   },
